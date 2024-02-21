36b7871a9e61a80877f4cb31d999cd71d4911259
dm2998 <diego2998@gamil.com>
2024-02-21 13:10:04 +0000

    Project WorkPlacement

---
+++ WebApplication2/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Data.Entity;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)  
+        {
+
+        }
+    }
+}
